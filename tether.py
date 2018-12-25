6dc26d3f5464ec742e436e7608ffe1d8f04cb43a
thomasekyle <thomasekyle@gmail.com>
2018-12-25 14:38:18 -0500

    Adding the base code.

---
+++ tether.py
@@ -0,0 +1,27 @@
+#!/bin/env/python
+
+import os
+import sys
+import getpass
+import json
+import paramiko
+
+def load_configuration(configuration):
+    return json.loads(configuration)
+    pass
+
+def run_configuration(client, configurations):
+    pass
+
+def get_connection(server, user, password, *args, **kwargs):
+    pass
+
+def run_command(client, command):
+    pass
+
+def main(argv):
+    pass
+
+
+if __name__ == "__main__":
+    main(sys.argv)
